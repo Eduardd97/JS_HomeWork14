adf9e6fcdff9f35f33a5b843cfa2306a5959d872
Eduard <kozeychukk1997@gmail.com>
2023-11-15 23:05:55 +0200

    deleteButton

--- main.mjs
+++ main.mjs
@@ -1,6 +1,8 @@
 import { faker } from "https://cdn.skypack.dev/@faker-js/faker";
 import { v4 as uuidv4 } from 'https://cdn.skypack.dev/uuid';
 
+
+
 const userQuantity = 100;
 
 function generateUser() {
@@ -65,28 +67,43 @@ searchButton.onclick = () => {
     }
 };
 
-
-
 function renderUsers (users) {
     usersWrapper.innerHTML = "";
 
-    users.forEach((users) =>{
-        const { fullname, city, location, pictureURL, salary, technologies } = users;
-        usersWrapper.innerHTML += `
-            
-        <div class="user-item">
-            <img alt="${fullname}" src="${pictureURL}" class="user-avatar"/>
-            <div>
-                <h3>${fullname}</h3>
-                <h4>${salary}$</h4>
-                <span>${location}, ${city}</span>
+    users.forEach((user, i) =>{
+        const {id, fullname, city, location, pictureURL, salary, technologies } = user;
+        usersWrapper.innerHTML += 
+        `
+            <div class="user-item" id="user-${id}">
+                <img alt="${fullname}" src="${pictureURL}" class="user-avatar"/>
+                <div>
+                    <h3>${fullname}</h3>
+                    <h4>${salary}$</h4>
+                    <span>${location}, ${city}</span>
+                </div>
+                <div class="technologies">
+                    ${technologies.map((tech) => `<span>${tech}</span>`).join("")}
+                </div>
             </div>
-            <div class="technologies">
-                ${technologies.map((tech) => `<span>${tech}</span>`).join("")}
-            </div>
-        </div>
         `;
-    })
+
+        setTimeout(() => {
+            const deleteButton = document.createElement("button");     
+            deleteButton.textContent = "Delete";
+      
+            deleteButton.onclick = () => {
+                console.log(`Delete user:`, user);
+            
+                // HW
+                const updatedUsers = USERS.filter((deleteUserById) => deleteUserById.id !== id);
+                
+                renderUsers(updatedUsers);                               
+            };
+      
+            const userElement = document.getElementById(`user-${id}`);
+            userElement.appendChild(deleteButton);
+        }, 1000);
+    });
 }
 
 
